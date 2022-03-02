b9471cc6236cc86d1a2a36d9f58ced089b5a4838
90150N <mugirethe1@gmail.com>
2022-03-02 12:38:41 +0300

    feat: show different texts in career class

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
+// navbar color change on scroll
 document.addEventListener("scroll", () => {
   let nav = document.querySelector(".navbar");
   nav.classList.toggle("solid-scroll", window.scrollY > 0);
 });
+
+// typing effect on career
+let names = [
+  "Software Developer",
+  "UI/UX Designer",
+  "Web Developer",
+  "Software Engineer",
+  "Programmer",
+];
+let inputArea = document.querySelector(".career");
+let counter = 0;
+let careerIndex = 0;
+let displayText = "";
+let letter = "";
+
+(typer = () => {
+  if (counter === names.length) {
+    counter = 0;
+  }
+  displayText = names[counter];
+  letter = displayText.slice(0, ++careerIndex);
+  inputArea.innerText = letter;
+
+  if (letter.length === displayText.length) {
+    counter++;
+    careerIndex = 0;
+  }
+
+  setTimeout(typer, 300);
+})();
